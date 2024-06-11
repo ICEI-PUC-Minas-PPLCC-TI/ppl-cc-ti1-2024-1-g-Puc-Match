91038a16ab192fdb703697beba06d6536d66f011
Diego Cheib <diegocheib@gmail.com>
2024-06-10 23:10:50 -0300

    feat: Ajustado commands.sh e requirements.txt

--- app/commands.sh
+++ app/commands.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+python -m venv venv
 
 # Ativa o ambiente virtual
 source venv/bin/activate
 
-# Navega até a pasta do projeto Django
-cd ti-1-ppl-cc-m-20241-3-ti-1-ppl-cc-m-20241-3/app
 
 # Instala todas dependências do projeto
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Executa as migrações
 python manage.py collectstatic --noinput --clear
